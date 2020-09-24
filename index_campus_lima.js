2af00d99b5039ac6544a27759dbab42490223ff1
josmig <pantigosomiguel59@gmail.com>
2020-09-23 22:25:51 -0500

    extracción de datos fce y facihed

--- index_campus_lima.js
+++ index_campus_lima.js
@@ -4,7 +4,7 @@ const fs = require('fs-extra');
 
 const writeStream = fs.createWriteStream('empresariales.csv');
 
-//Informacion Facultad
+//Informacion sobre todas las facultades
 async function init(){
 	//$ -> Simboliza un objeto
 	const $ = await request({
@@ -15,48 +15,61 @@ async function init(){
 		transform: body => cheerio.load(body)
 	});
 	//console.log($)
-		
+
 	//Extrayendo el titulo
-	
+
 	console.log('******Facultades Y Descripcion Campus******\n');
 	const campus_description= $('.aio-icon-description');
 	const facultades_central = $('h3.aio-icon-title').each((i,e)=>{
 
-		//const facu = facultades_central.text();	
+		//const facu = facultades_central.text();
 		const facu = $(e).text();
 		const fac = facu.replace(""," ");
-		console.log(fac.trim())	
+		console.log(fac.trim())
 	});
-		
+
 	//	con .find('a') --> buscamos los enlaces
-	//	const campus= $('.ult-new-ib-title').find('a');	
+	//	const campus= $('.ult-new-ib-title').find('a');
 	//	Para ver las etiquetas padres --> parent()  Para recorrer --> next() Para ver las etiquetas hijas --> children
-	
+
 	//console.log(campus.html());
 	console.log(campus_description.text());
 	//console.log(carreras.text());
-	
-}	
-init();
+
+}
+//init();
 
 
 //informacion de toda FCE
 async function fce(){
 	const $ = await request.get({
 		uri:'https://www.upeu.edu.pe/fce/',
-		uri:'https://www.upeu.edu.pe/fce/bienvenida/',
 		transform: body => cheerio.load(body)
 	});
-		//const carrerasfce = $('.menu-item-text .menu-text').text('Carreras');
-	//const carrerasfce= $('nav');
-	// const carrerasfce = $('nav span.menu-text').each((indice,elemento) => {
-	// 	console.log(indice,$(elemento).text());
 
-	// })	
-	// console.log(carrerasfce.text().eq(7).trim());
-	
-	//carreras
-	console.log('*****carreras******\n');
+	//Info de la facultad
+	$('.uvc-heading').each((i,el)=>{
+		const titulo_1 = $(el).find('.uvc-main-heading').text();
+		const parrafo = $(el).find('p').text();
+		if( i === 0){
+			console.log('**** MOtivación ****');
+			console.log(titulo_1);
+			console.log(parrafo);
+		}else if(i === 1){
+			console.log('******Palabras decana****');
+			console.log(titulo_1);
+			$('.vc_column-inner').each((i,el)=>{
+				const parrafo = $(el).find('p').text()
+				if(i=== 1){
+					console.log(parrafo);
+				}
+			})
+		}
+	})
+
+
+	//carreras FCE
+	console.log('*****carreras FCE******\n');
 		writeStream.write('Carreras')
 		$('nav').each((i,el)=>{
 			let carreras = [];
@@ -64,26 +77,57 @@ async function fce(){
 			const items2 = $(el).find('li .menu-item-1012').text();
 			const items3 = $(el).find('li .menu-item-1013').text();
 			const items4 = $(el).find('li .menu-item-846').text();
-	
-			
+
 			//carreras.push(items1,items2,items3)
 			carreras.push(items1,items2,items3,items4);
-	
+
 			//console.log(carreras);
 			writeStream.write(`${carreras}\n`);
 			carreras.forEach((e,i)=>{
 				//+'\n'
 				console.log(e + '\n');
-			
-			});	
-		})
-	
-		
-	console.log('**************bienvenida**************\n');
-	const saludo= $('.wpb_content_element')
-	const img= $('.vc_figure').find('.vc_single_image-wrapper').find('img').attr('src')	
-	console.log(saludo.text().trim());		
-	console.log(img);
-}
 
-fce();
+			});
+		})
+}
+//fce();
+
+async function facihed(){
+	const $ = await request.get({
+		uri:'https://www.upeu.edu.pe/facihed/',
+		uri:'https://www.upeu.edu.pe/facihed/vision/',
+		transform: body => cheerio.load(body)
+	});
+
+	//Mision y vision
+	console.log('***MISION Y VISION');
+	$('.content').each((i,el)=>{
+			const titulos = $(el).find('h3').text();
+			const parrafo = $(el).find('span').text();
+			console.log(i,titulos);
+			console.log(parrafo);
+
+	})
+
+
+	//carreras FCE
+	console.log('*****carreras FACIHED******\n');
+		writeStream.write('Carreras')
+		$('.main-nav').each((i,el)=>{
+			let carreras = [];
+			const items1 = $(el).find('li .menu-item-962').text();
+			const items2 = $(el).find('li .menu-item-1058').text();
+
+			//carreras.push(items1,items2,items3)
+			carreras.push(items1,items2);
+
+			//console.log(carreras);
+			writeStream.write(`${carreras}\n`);
+			carreras.forEach((e,i)=>{
+				//+'\n'
+				console.log(e + '\n');
+
+			});
+		})
+}
+facihed();
